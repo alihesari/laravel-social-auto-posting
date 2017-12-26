6950a4d3fb29a9c4c4ad9d6006c25be13b14c597
Ali Hesari <totaldesign.ir@gmail.com>
2017-12-26 21:01:59 +0330

    Add tests

---
+++ tests/SendToTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class SendToTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
