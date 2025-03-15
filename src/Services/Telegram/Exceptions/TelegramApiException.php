b43223e855c52192d615e8e6ee6040f92116714f
Ali Hesari <alihesari.com@gmail.com>
2025-03-16 00:37:54 +0300

    feat(telegram): add custom exception class

---
+++ src/Services/Telegram/Exceptions/TelegramApiException.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Toolkito\Larasap\Services\Telegram\Exceptions;
+
+class TelegramApiException extends \Exception
+{
+    protected $httpCode;
+    protected $errorCode;
+    protected $parameters;
+
+    public function __construct($message, $httpCode = 0, $errorCode = null, $parameters = null)
+    {
+        parent::__construct($message);
+        $this->httpCode = $httpCode;
+        $this->errorCode = $errorCode;
+        $this->parameters = $parameters;
+    }
+
+    public function getHttpCode()
+    {
+        return $this->httpCode;
+    }
+
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
+
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+} 
