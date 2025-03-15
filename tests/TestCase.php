31669c6ed6a2a0d93fbf2b705343da14595b6ecb
Ali Hesari <alihesari.com@gmail.com>
2025-03-16 00:04:09 +0300

    test: update TestCase class with HTTP facade and config setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,28 +14,36 @@ class TestCase extends BaseTestCase
         parent::setUp();
         
         // Set up config values
-        config([
-            'larasap.telegram.channel_signature' => '',
+        $this->app['config']->set([
+            'larasap.telegram.api_token' => '123456789:test_token',
+            'larasap.telegram.bot_username' => 'test_bot',
+            'larasap.telegram.channel_username' => '@test_channel',
+            'larasap.telegram.channel_signature' => '- Test Signature',
+            'larasap.telegram.proxy' => false,
+
             'larasap.x.consumer_key' => 'test_key',
             'larasap.x.consumer_secret' => 'test_secret',
             'larasap.x.access_token' => 'test_token',
             'larasap.x.access_token_secret' => 'test_token_secret',
-            'larasap.facebook.app_id' => 'test_app_id',
+
+            'larasap.facebook.app_id' => '123456789',
             'larasap.facebook.app_secret' => 'test_app_secret',
-            'larasap.facebook.access_token' => 'test_access_token',
+            'larasap.facebook.access_token' => 'EAAxxxxx',
+            'larasap.facebook.page_id' => '123456789',
+            'larasap.facebook.default_graph_version' => 'v18.0',
         ]);
 
         // Mock HTTP requests
         Http::fake([
             // Mock Telegram API requests
-            'https://api.telegram.org/bot*' => Http::response(['ok' => true], 200),
+            'https://api.telegram.org/bot123456789:test_token/*' => Http::response(['ok' => true, 'result' => ['message_id' => 123]], 200),
             
             // Mock X API requests
-            'https://api.x.com/1.1/*' => Http::response(['id' => '123456789'], 200),
+            'https://api.x.com/2/*' => Http::response(['id' => '123456789'], 200),
             'https://upload.x.com/1.1/*' => Http::response(['media_id_string' => '123456789'], 200),
             
             // Mock Facebook API requests
-            'https://graph.facebook.com/*' => Http::response(['id' => '123456789'], 200),
+            'https://graph.facebook.com/*' => Http::response(['id' => '123456789_123456789'], 200),
         ]);
     }
 
@@ -56,6 +64,7 @@ class TestCase extends BaseTestCase
             'prefix'   => '',
         ]);
 
+        // Set up package configuration
         $app['config']->set('larasap.x.consumer_key', 'test_key');
         $app['config']->set('larasap.x.consumer_secret', 'test_secret');
         $app['config']->set('larasap.x.access_token', 'test_token');
