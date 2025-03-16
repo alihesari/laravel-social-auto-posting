dde78557fe65e07cde8cd67196012bdd68ccdb41
Ali Hesari <alihesari.com@gmail.com>
2025-03-16 11:28:12 +0300

    feat: add example test route (fixes #43)
    - Add test-social-posting route - Link to SocialMediaController - Add proper route naming

--- examples/laravel-12/routes/web.php
+++ examples/laravel-12/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SocialMediaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-social-posting', [SocialMediaController::class, 'test'])
+    ->name('social.test');
