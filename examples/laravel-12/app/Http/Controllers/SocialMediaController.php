c839390e4b0b2b9804253252b655866444c7a7bb
Ali Hesari <alihesari.com@gmail.com>
2025-03-16 13:02:16 +0300

    chore(#43): add Laravel 12 example project for development and testing

--- examples/laravel-12/app/Http/Controllers/SocialMediaController.php
+++ examples/laravel-12/app/Http/Controllers/SocialMediaController.php
@@ -3,23 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Toolkito\Larasap\Facades\X;
-use Toolkito\Larasap\Facades\Telegram;
-use Toolkito\Larasap\Facades\Facebook;
+use Toolkito\Larasap\SendTo;
 
 class SocialMediaController extends Controller
 {
-    public function test()
+    /**
+     * Test Telegram posting
+     */
+    public function testTelegram()
+    {
+        try {
+            $result = SendTo::telegram('Test message from Laravel 12');
+            return response()->json(['telegram' => $result]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Test X (Twitter) posting
+     */
+    public function testX()
+    {
+        try {
+            $result = SendTo::x('Test tweet from Laravel 12');
+            return response()->json(['x' => $result]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Test Facebook posting
+     */
+    public function testFacebook()
+    {
+        try {
+            $result = SendTo::facebook([
+                'message' => 'Test post from Laravel 12'
+            ]);
+            return response()->json(['facebook' => $result]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Test all social media platforms
+     */
+    public function testAll()
     {
         try {
             // Test Telegram posting
-            $telegramResult = Telegram::sendMessage('Test message from Laravel 12');
+            $telegramResult = SendTo::telegram('Test message from Laravel 12');
             
             // Test X (Twitter) posting
-            $xResult = X::post('Test tweet from Laravel 12');
+            $xResult = SendTo::x('Test tweet from Laravel 12');
             
             // Test Facebook posting
-            $facebookResult = Facebook::post([
+            $facebookResult = SendTo::facebook([
                 'message' => 'Test post from Laravel 12'
             ]);
             
--- examples/laravel-12/config/app.php
+++ examples/laravel-12/config/app.php
@@ -123,4 +123,89 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'providers' => [
+        /*
+         * Laravel Framework Service Providers...
+         */
+        Illuminate\Auth\AuthServiceProvider::class,
+        Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        Illuminate\Bus\BusServiceProvider::class,
+        Illuminate\Cache\CacheServiceProvider::class,
+        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+        Illuminate\Cookie\CookieServiceProvider::class,
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+        Illuminate\Hashing\HashServiceProvider::class,
+        Illuminate\Mail\MailServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
+        Illuminate\Pagination\PaginationServiceProvider::class,
+        Illuminate\Pipeline\PipelineServiceProvider::class,
+        Illuminate\Queue\QueueServiceProvider::class,
+        Illuminate\Redis\RedisServiceProvider::class,
+        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
+        Illuminate\Session\SessionServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class,
+        Illuminate\Validation\ValidationServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
+
+        /*
+         * Package Service Providers...
+         */
+        Toolkito\Larasap\LarasapServiceProvider::class,
+
+        /*
+         * Application Service Providers...
+         */
+        App\Providers\AppServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
+    ],
+
+    'aliases' => [
+        'App' => Illuminate\Support\Facades\App::class,
+        'Arr' => Illuminate\Support\Arr::class,
+        'Artisan' => Illuminate\Support\Facades\Artisan::class,
+        'Auth' => Illuminate\Support\Facades\Auth::class,
+        'Blade' => Illuminate\Support\Facades\Blade::class,
+        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
+        'Bus' => Illuminate\Support\Facades\Bus::class,
+        'Cache' => Illuminate\Support\Facades\Cache::class,
+        'Config' => Illuminate\Support\Facades\Config::class,
+        'Cookie' => Illuminate\Support\Facades\Cookie::class,
+        'Crypt' => Illuminate\Support\Facades\Crypt::class,
+        'Date' => Illuminate\Support\Carbon::class,
+        'DB' => Illuminate\Support\Facades\DB::class,
+        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
+        'Event' => Illuminate\Support\Facades\Event::class,
+        'File' => Illuminate\Support\Facades\File::class,
+        'Gate' => Illuminate\Support\Facades\Gate::class,
+        'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Http' => Illuminate\Support\Facades\Http::class,
+        'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Log' => Illuminate\Support\Facades\Log::class,
+        'Mail' => Illuminate\Support\Facades\Mail::class,
+        'Notification' => Illuminate\Support\Facades\Notification::class,
+        'Password' => Illuminate\Support\Facades\Password::class,
+        'Process' => Illuminate\Support\Facades\Process::class,
+        'Queue' => Illuminate\Support\Facades\Queue::class,
+        'RateLimiter' => Illuminate\Support\Facades\RateLimiter::class,
+        'Redirect' => Illuminate\Support\Facades\Redirect::class,
+        'Request' => Illuminate\Support\Facades\Request::class,
+        'Response' => Illuminate\Support\Facades\Response::class,
+        'Route' => Illuminate\Support\Facades\Route::class,
+        'Schema' => Illuminate\Support\Facades\Schema::class,
+        'Session' => Illuminate\Support\Facades\Session::class,
+        'Storage' => Illuminate\Support\Facades\Storage::class,
+        'Str' => Illuminate\Support\Str::class,
+        'URL' => Illuminate\Support\Facades\URL::class,
+        'Validator' => Illuminate\Support\Facades\Validator::class,
+        'View' => Illuminate\Support\Facades\View::class,
+        'Telegram' => Toolkito\Larasap\Facades\Telegram::class,
+        'X' => Toolkito\Larasap\Facades\X::class,
+        'Facebook' => Toolkito\Larasap\Facades\Facebook::class,
+    ],
+
 ];
